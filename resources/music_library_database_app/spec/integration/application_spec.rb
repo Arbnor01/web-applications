cd28d9d2964e94f682d184686cfeb9966b9198ec
Arbnor <arbnor-sfarca@hotmail.com>
2023-06-01 11:04:22 +0100

    Exercise: Added test case for creating a new album via POST request

--- resources/music_library_database_app/spec/integration/application_spec.rb
+++ resources/music_library_database_app/spec/integration/application_spec.rb
@@ -10,4 +10,20 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+
+  context 'POST /albums' do
+    it 'should create a new album' do
+      # Send a POST request to the '/albums' endpoint with the necessary parameters
+      response = post(
+        '/albums',
+        title: 'OK Computer', 
+        release_year: '1997', 
+        artist_id: '1'
+      )
+       # Verify that the response status is 200 (OK)
+      expect(response.status).to eq(200)
+      # Verify that the response body is empty
+      expect(response.body).to eq('')
+    end
+  end
 end
